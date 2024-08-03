802e520d375f22a449500200b5183d0c24f7d48a
Amr <amrkhaled12338@gmail.com>
2024-08-03 19:34:47 +0300

    fix search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,59 +1,47 @@
-"use client"
+"use client";
 
-import * as z from "zod"
-import { useRouter } from "next/navigation"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-  } from "@/components/ui/form"
-import { Input } from "./ui/input"
+import * as z from "zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "./ui/input";
 
 const formSchema = z.object({
-    input: z.string().min(2).max(50),
-})
+  input: z.string().min(2).max(50),
+});
 
 const SearchBar = () => {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      input: "",
+    },
+  });
 
-    const router = useRouter()
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            input: "",
-        },
-    })
-
-    function onSubmit(values: z.infer<typeof formSchema>) {
-         console.log(values)
-         router.push(`/search/${values.input}`)
-    }
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    router.push(`/search/${values.input}`);
+    form.reset();
+  }
 
   return (
     <Form {...form}>
-      <form 
-        className=" space-y-8"
-        onSubmit={form.handleSubmit(onSubmit)}
-      >
-        <FormField 
+      <form className=" space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
           control={form.control}
           name="input"
           render={({ field }) => (
             <FormItem>
               <FormControl>
-                <Input 
-                  placeholder="Search..."
-                  {...field} 
-                />
+                <Input placeholder="Search..." {...field} />
               </FormControl>
             </FormItem>
           )}
         />
       </form>
     </Form>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
