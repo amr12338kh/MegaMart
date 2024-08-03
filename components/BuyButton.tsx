1c448b6c82dfde6b71731b9046d2872697d2aefa
Amr <amrkhaled12338@gmail.com>
2024-08-03 19:20:43 +0300

    adjust buy btn

--- components/BuyButton.tsx
+++ components/BuyButton.tsx
@@ -1,43 +1,39 @@
-"use client"
+"use client";
 
-import { useShopingCart } from "@/context/ShopingCartProvider"
-import { Button } from "./ui/button"
-import { ProductProps } from "@/types"
-import { useRouter } from "next/navigation"
+import { useShopingCart } from "@/context/ShopingCartProvider";
+import { Button } from "./ui/button";
+import { ProductProps } from "@/types";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
 
 interface Props {
-    product: ProductProps
+  product: ProductProps;
 }
 
 const BuyButton = ({ product }: Props) => {
+  const [isClicked, setIsClicked] = useState(false);
+  const router = useRouter();
+  const { increaseCartQuantity, getItemQuantity } = useShopingCart();
+  const quantity = getItemQuantity(product.id);
 
-    const router = useRouter()
-
-    const { 
-        increaseCartQuantity, 
-        getItemQuantity, 
-    } = useShopingCart()
-
-    const quantity = getItemQuantity(product.id)
-
-    const handelBuyButton = () => {
-        if (quantity < 1) {
-            increaseCartQuantity(product.id)
-            router.replace("/cart")
-        } else {
-            router.replace("/cart")
-        }
+  const handleBuyButton = () => {
+    setIsClicked(true);
+    if (quantity < 1) {
+      increaseCartQuantity(product.id);
     }
-    
-  return (
-    <Button
-        variant="default"
-        size="sm"
-        onClick={handelBuyButton}
-    >
-        Buy now
-    </Button>
-  )
-}
+    router.replace("/cart");
+  };
 
-export default BuyButton
+  return isClicked ? (
+    <Button variant="default" size="sm" disabled>
+      <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Loading...
+    </Button>
+  ) : (
+    <Button variant="default" size="sm" onClick={handleBuyButton}>
+      Buy now
+    </Button>
+  );
+};
+
+export default BuyButton;
